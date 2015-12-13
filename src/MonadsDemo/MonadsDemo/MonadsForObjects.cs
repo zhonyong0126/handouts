e9d4901459587cdc5014b3adac832196132a0bad
Peter Gao <roc.gao@msn.cn>
2015-12-13 21:32:48 +0800

    添加源代码

---
+++ src/MonadsDemo/MonadsDemo/MonadsForObjects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Monads;
+
+namespace MonadsDemo
+{
+    class MonadsForObjects
+    {
+        public static void DoDemo()
+        {
+            var name = "gao";
+            if (null != name)
+            {
+                Console.WriteLine($"[常规] name不为Null, 所以打印name:{name}");
+            }
+            name.Do(_ => Console.WriteLine($"[Monads] name不为Null, 所以打印name:{_}"));
+        }
+    }
+}
---
+++ src/MonadsDemo/MonadsDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Monads;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonadsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MonadsForObjects.DoDemo();
+
+            var data = new Dictionary<int, string>();
+            //data.Add(1, "My Name");
+            //data.With(1).Do(_ => Console.WriteLine($"已找到Key为1的结果：{_}"));
+            data.With(1).Return(_ => $"已找到Key为1的结果：{_}", "未找到Key为1的结果").Do(_ => Console.WriteLine(_));
+           data.Return(1, _ =>
+           {
+               Console.WriteLine("Key1不存在，添加Key1");
+               _.
+           })
+            //data.TryWith(1)
+            Console.Read();
+        }
+    }
+}
